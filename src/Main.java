c9eb2bac3dae39d4a3dce0cae71c77d4e9096ff3
4eJloBe4eK <dmitruk.kirill16@gmail.com>
2025-12-12 02:12:12 +0300

    Конфликт

--- src/Main.java
+++ src/Main.java
@@ -176,7 +176,7 @@ public class Main {
 
         int choice = readInt();
         if (choice < 1 || choice > clients.size()) {
-            System.out.println("Неверный выбор.");
+            System.out.println("Ошибка.");
             return;
         }
 
